1eaba5cc772853391fc88edbbc2004c0580b5cd2
dzgcsmthmt <dzgcsmthmt@users.noreply.github.com>
2018-11-10 22:25:36 +0800

    判断是否支持某事件

--- js/feature-detection.js
+++ js/feature-detection.js
@@ -10,3 +10,23 @@ var hasWebP = false;
   };
   img.src = 'data:image/webp;base64,UklGRh4AAABXRUJQVlA4TBEAAAAvAQAAAAfQ//73v/+BiOh/AAA=';
 })();
+//判断是否支持某事件
+var isEventSupported = (function(){
+  var TAGNAMES = {
+    'select':'input','change':'input',
+    'submit':'form','reset':'form',
+    'error':'img','load':'img','abort':'img'
+  }
+  function isEventSupported(eventName) {
+    var el = document.createElement(TAGNAMES[eventName] || 'div');
+    eventName = 'on' + eventName;
+    var isSupported = (eventName in el);
+    if (!isSupported) {
+      el.setAttribute(eventName, 'return;');
+      isSupported = typeof el[eventName] == 'function';
+    }
+    el = null;
+    return isSupported;
+  }
+  return isEventSupported;
+})();
