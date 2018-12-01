1f5cb4aee31fbdc79303c6e3522144e78e959869
xuhao <xuhao@okjiaoyu.cn>
2018-12-01 11:32:48 +0800

    reconstructor

--- my-app/src/todoAppRedux/reducers/index.js
+++
@@ -1,5 +0,0 @@
-import { combineReducers } from 'redux';
-import todos from './todo-reducer';
-import filter from './filter-reducer';
-
-export default combineReducers({todos,filter});
--- polyfill.js
+++ polyfill.js
@@ -21,4 +21,33 @@ function round(value, exp) {
   return +(value[0] + 'e' + (value[1] ? (+value[1] - exp) : -exp));
 }
 
-round(2.005, 2); 
+round(2.005, 2);
+
+//Function.prototype.bind
+if (!Function.prototype.bind) {
+  Function.prototype.bind = function(oThis) {
+    if (typeof this !== 'function') {
+      // closest thing possible to the ECMAScript 5
+      // internal IsCallable function
+      throw new TypeError('Function.prototype.bind - what is trying to be bound is not callable');
+    }
+
+    var aArgs   = Array.prototype.slice.call(arguments, 1),
+        fToBind = this,
+        fNOP    = function() {},
+        fBound  = function() {
+          return fToBind.apply(this instanceof fNOP
+                 ? this
+                 : oThis,
+                 aArgs.concat(Array.prototype.slice.call(arguments)));
+        };
+
+    if (this.prototype) {
+      // Function.prototype doesn't have a prototype property
+      fNOP.prototype = this.prototype;
+    }
+    fBound.prototype = new fNOP();
+
+    return fBound;
+  };
+}
