a06ef69e4655388c915e1452cdc1392e6ceea785
xuhao <xuhao@okay.cn>
2021-05-27 11:34:35 +0800

    add escape less than sign method

--- js/util.js
+++ js/util.js
@@ -161,5 +161,57 @@ var util = {
     },
     delCookie: function(cname) {
         this.setCookie(cname,"",-1);
+    },
+    escapeLtSign: function(str){
+        let allTags =   ('html,body,base,head,link,meta,style,title,' +
+                        'address,article,aside,footer,header,h1,h2,h3,h4,h5,h6,hgroup,nav,section,' +
+                        'div,dd,dl,dt,figcaption,figure,picture,hr,img,li,main,ol,p,pre,ul,' +
+                        'a,b,abbr,bdi,bdo,br,cite,code,data,dfn,em,i,kbd,mark,q,rp,rt,rtc,ruby,' +
+                        's,samp,small,span,strong,sub,sup,time,u,var,wbr,area,audio,map,track,video,' +
+                        'embed,object,param,source,canvas,script,noscript,del,ins,' +
+                        'caption,col,colgroup,table,thead,tbody,td,th,tr,' +
+                        'button,datalist,fieldset,form,input,label,legend,meter,optgroup,option,' +
+                        'output,progress,select,textarea,' +
+                        'details,dialog,menu,menuitem,summary,' +
+                        'content,element,shadow,template,blockquote,iframe,tfoot' + 
+                        'svg,animate,circle,clippath,cursor,defs,desc,ellipse,filter,font-face,' +
+                        'foreignObject,g,glyph,image,line,marker,mask,missing-glyph,path,pattern,' +
+                        'polygon,polyline,rect,switch,symbol,text,textpath,tspan,use,view').split(',');
+        let tagName = '';
+        let len = str.length;
+        let stack = [];
+        let res = str;
+        let startFlag = false;
+        for(let i = 0;i < len;i++){
+            if(str[i] == '<'){
+                startFlag = true;
+                stack.push({pos:i});
+            }else if(str[i] == '>'){
+                if(startFlag){
+                    startFlag = false;
+                    stack.pop();
+                }
+            }else{
+                if(startFlag){
+                    if(/[a-zA-Z\-]/.test(str[i])){
+                        tagName += str[i];
+                    }else{
+                        if(!tagName || allTags.indexOf(tagName) > -1){
+                            stack.pop();
+                            startFlag = false;
+                        }
+                        tagName = '';
+                    }
+                }
+            }
+        }
+
+        // console.log(stack);
+        for(var j = stack.length - 1;j >= 0;j--){
+            let pos = stack[j].pos;
+            res = res.substring(0,pos) + '&lt;' + res.substring(pos + 1);
+        }
+
+        return res;
     }
 }
