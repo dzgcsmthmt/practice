67c35dba26623a895629f82c581248093a1a061f
xuhao <xuhao@okay.cn>
2021-08-05 11:15:33 +0800

    fix return statement

--- babel-plugin/benchmark.js
+++ babel-plugin/benchmark.js
@@ -1,29 +1,38 @@
-function a(){
-	let count = 0;
-  	for(let i = 0;i < 100;i++){
-    	count += i;
-    }
+function a() {
+  let count = 0;
+  for (let i = 0; i < 100; i++) {
+    count += i;
+  }
 }
 a();
 
 export default function (babel) {
-    const { types: t } = babel;
-    
-    return {
-      name: "ast-transform", // not required
-      visitor: {
-        FunctionDeclaration(path){
-            console.log(path.node.id);
-          let name = path.node.id.name;
-          //path.get('body').unshiftContainer('body', t.expressionStatement(t.stringLiteral('before')))
-          path.get('body').unshiftContainer('body', t.callExpression(t.MemberExpression(t.Identifier('console'),t.Identifier('time')),[t.StringLiteral(name)]))
-          //path.get('body').pushContainer('body', t.expressionStatement(t.stringLiteral('after')))
-          path.get('body').pushContainer('body', t.callExpression(t.MemberExpression(t.Identifier('console'),t.Identifier('timeEnd')),[t.StringLiteral(name)]))
-        },
-        CallExpression(path){
-            console.log(path)
+  const { types: t } = babel;
+
+  return {
+    name: "ast-transform", // not required
+    visitor: {
+      FunctionDeclaration(path) {
+        console.log(path.node.id);
+        let name = path.node.id.name;
+        console.log(path.get('body'));
+        const body = path.get('body');
+        const last = body.get('body').pop();
+        console.log(last, t.isReturnStatement(last))
+
+        if (t.isReturnStatement(last)) {
+          last.insertBefore(t.callExpression(t.MemberExpression(t.Identifier('console'), t.Identifier('timeEnd')), [t.StringLiteral(name)]));
+        } else {
+          path.get('body').pushContainer('body', t.callExpression(t.MemberExpression(t.Identifier('console'), t.Identifier('timeEnd')), [t.StringLiteral(name)]))
         }
+        //path.get('body').unshiftContainer('body', t.expressionStatement(t.stringLiteral('before')))
+        //path.get('body').unshiftContainer('body', t.callExpression(t.MemberExpression(t.Identifier('console'), t.Identifier('time')), [t.StringLiteral(name)]))
+        //path.get('body').pushContainer('body', t.expressionStatement(t.stringLiteral('after')))
+        //path.get('body').pushContainer('body', t.callExpression(t.MemberExpression(t.Identifier('console'), t.Identifier('timeEnd')), [t.StringLiteral(name)]))
+      },
+      CallExpression(path) {
+        console.log(path)
       }
-    };
-  }
-  
+    }
+  };
+}
