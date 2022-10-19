0b56f120427e676cdf1bf966fd42c51e979d767b
dzgcsmthmt <dzgcsmthmt@users.noreply.github.com>
2022-10-19 14:28:58 +0800

    Create number.js

---
+++ js/number.js
@@ -0,0 +1,27 @@
+function int2Binary(num){
+    let res = '';
+    while(num){
+        res = (num & 1) + res;
+        num = Math.floor(num / 2);
+    }
+    return res;
+}
+
+function float2Binary(num){
+    let res = '';
+    while(num){
+        num = num * 2;
+        res += (num & 1);
+        num = num % 1;
+        console.log(num);
+    }
+    return res;
+}
+
+const num = 2 ** 53;
+console.log(int2Binary(num));
+console.log(num.toString(2));
+
+const fNum = 0.2;
+console.log('0.' + float2Binary(fNum));
+console.log(fNum.toString(2));
