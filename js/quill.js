5b2f38add0cb9c2ec554b46c6e96fd7c29d46d17
dzgcsmthmt <dzgcsmthmt@users.noreply.github.com>
2024-07-25 14:19:25 +0800

    feat: add new custom className

---
+++ js/quill.js
@@ -0,0 +1,21 @@
+//add new custom className
+let Block = Quill.import("blots/block");
+
+// Create a custom blot for Javascript
+class CustomBlot extends Block {
+    static create(url) {
+        let node = super.create();
+        // Attribute for output HTML
+        // node.setAttribute("class", "custom-class");
+        return node;
+    }
+}
+
+// The format name to toggle
+CustomBlot.blotName = "custom";
+// Class to look for when parsing your input HTML
+CustomBlot.className = "custom-class";
+// Tag to look for when parsing your input HTML
+CustomBlot.tagName = "div";
+// Register your custom blot with quill.
+Quill.register(CustomBlot);
